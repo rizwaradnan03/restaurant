7510026b309fa83ef3b2cbd918ac2c25a905a024
Rizwar Adnanbyantara <rizwarbyan123@gmail.com>
2024-08-19 02:21:31 +0700

    Unfinished Sidebar Component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
+import MainLayouts from "@/layouts/MainLayout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <MainLayouts>
+        <Component {...pageProps} />
+      </MainLayouts>
+    </>
+  );
 }
